661fecb4b1c6c07fd92a2369800bbc2e40104a31
tulika03 <tulika312@gmail.com>
2018-04-01 20:10:11 +0530

    complain, inquiry

--- src/router/adminindex.js
+++ src/router/adminindex.js
@@ -4,10 +4,13 @@ import viewFaculty from '@/components/admin/Faculty/viewFaculty'
 import add from '@/components/admin/Faculty/add'
 import update from '@/components/admin/Faculty/update'
 import view from '@/components/admin/Faculty/view'
-import studentview from '@/components/admin/student/view/'
+import studentview from '@/components/admin/student/sview/'
 import create from '@/components/admin/course/create'
 import removeCourse from '@/components/admin/course/remove'
-
+import AdminListComplains from '@/components/admin/complain/listcomplains'
+import AdminviewComplain from '@/components/admin/complain/viewComplain'
+import AdminListInquiries from '@/components/admin/inquiry/showInquiries'
+import AdminviewInquiry from '@/components/admin/inquiry/viewInquiry'
 Vue.use(Router)
 
 export default new Router({
@@ -33,7 +36,7 @@ export default new Router({
       component: view
     },
     {
-      path: '/admin/student/view',
+      path: '/admin/student/sview',
       name: 'studentview',
       component: studentview
     },
@@ -46,6 +49,26 @@ export default new Router({
       path: '/admin/course/remove',
       name: 'removeCourse',
       component: removeCourse
+    },
+    {
+      path: '/admin/complain/listcomplains',
+      name: 'AdminListComplains',
+      component: AdminListComplains
+    },
+    {
+      path: '/admin/complain/viewComplain',
+      name: 'AdminviewComplain',
+      component: AdminviewComplain
+    },
+    {
+      path: '/admin/inquiry/showInquiries',
+      name: 'AdminListInquiries',
+      component: AdminListInquiries
+    },
+    {
+      path: '/admin/inquiry/viewInquiry',
+      name: 'AdminviewInquiry',
+      component: AdminviewInquiry
     }
   ],
   mode: 'history'
